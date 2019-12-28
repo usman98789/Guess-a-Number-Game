bd46647b81d8a3be6135694211893747cee7bcd5
Usman Siddiqui <usmansiddiqui@Usmans-MacBook-Pro.local>
2019-12-28 01:27:02 -0500

    reset and confirm buttons

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,1 +1,43 @@
 import React from "react";
+import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+
+const StartGameScreen = props => {
+	return (
+		<View style={styles.screen}>
+			<Text style={styles.title}>Start a New Game</Text>
+			<View style={styles.inputContain}>
+				<Text>Select a Number</Text>
+				<TextInput />
+				<View style={styles.buttonContain}>
+					<Button title="Reset" onPress={() => {}} />
+					<Button title="Confirm" onPress={() => {}} />
+				</View>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+		padding: 10,
+		alignItems: "center"
+	},
+	buttonContain: {
+		flexDirection: "row",
+		width: "100%",
+		justifyContent: "space-between",
+		paddingHorizontal: 15
+	},
+	title: {
+		fontSize: 20,
+		marginVertical: 10
+	},
+	inputContain: {
+		width: 300,
+		maxWidth: "80%",
+		alignItems: "center"
+	}
+});
+
+export default StartGameScreen;
